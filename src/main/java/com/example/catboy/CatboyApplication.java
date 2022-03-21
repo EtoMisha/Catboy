e2c6e7c0ff3404504d782abb634f778bb40defbc
EtoMisha <baikov.mv@gmail.com>
2022-03-21 21:44:05 +0300

    Initial commit

---
+++ src/main/java/com/example/catboy/CatboyApplication.java
@@ -0,0 +1,13 @@
+package com.example.catboy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatboyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatboyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/catboy/CatboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatboyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
