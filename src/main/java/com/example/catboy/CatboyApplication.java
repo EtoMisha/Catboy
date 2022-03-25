c8bb0f573cca20b57162daf5f0ed7a1e30f259f8
EtoMisha <80752218+EtoMisha@users.noreply.github.com>
2022-03-25 19:01:52 +0300

    Update CatboyApplication.java

--- src/main/java/com/example/catboy/CatboyApplication.java
+++ src/main/java/com/example/catboy/CatboyApplication.java
@@ -2,7 +2,7 @@ package com.example.catboy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
 @SpringBootApplication
 public class CatboyApplication {
 
@@ -10,4 +10,14 @@ public class CatboyApplication {
         SpringApplication.run(CatboyApplication.class, args);
     }
 
+}
+*/
+public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(Bot.getBot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
 }
