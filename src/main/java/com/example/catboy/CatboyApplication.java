c3770622bb1c78bb641ac59c5e33d30033d72b4a
EtoMisha <80752218+EtoMisha@users.noreply.github.com>
2022-03-25 19:06:20 +0300

    Update CatboyApplication.java

--- src/main/java/com/example/catboy/CatboyApplication.java
+++ src/main/java/com/example/catboy/CatboyApplication.java
@@ -12,12 +12,14 @@ public class CatboyApplication {
 
 }
 */
-public static void main(String[] args) {
-        ApiContextInitializer.init();
-        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
-        try {
-            telegramBotsApi.registerBot(Bot.getBot());
-        } catch (TelegramApiRequestException e) {
-            e.printStackTrace();
+public class CatboyApplication {
+    public static void main(String[] args) {
+    ApiContextInitializer.init();
+    TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+    try {
+        telegramBotsApi.registerBot(Bot.getBot());
+    } catch (TelegramApiRequestException e) {
+        e.printStackTrace();
         }
+    }
 }
